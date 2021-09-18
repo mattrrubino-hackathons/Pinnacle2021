f45d6a456175c32d8bda5be33c94997cd9938639
Matthew Rubino <mrrubino@buffalo.edu>
2021-09-18 11:49:42 -0400

    Add hand gesture detection

---
+++ Assets/Scripts/HandTracking.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class HandTracking : MonoBehaviour
+{
+    [SerializeField] float confidenceThreshold = 0.9f;
+    MLHandTracking.HandKeyPose[] gestures = new MLHandTracking.HandKeyPose[1];
+
+    bool rightClosed, leftClosed;
+
+    void Start()
+    {
+        MLHandTracking.Start();
+
+        gestures[0] = MLHandTracking.HandKeyPose.Fist;
+
+        MLHandTracking.KeyPoseManager.EnableKeyPoses(gestures, true, false);
+    }
+
+    void OnDestroy()
+    {
+        MLHandTracking.Stop();
+    }
+
+    void Update()
+    {
+        if (GetGesture(MLHandTracking.Left, MLHandTracking.HandKeyPose.Fist))
+        {
+            if (!leftClosed)
+            {
+                OnLeftClosed();
+            }
+            leftClosed = true;
+        }
+        else
+        {
+            leftClosed = false;
+        }
+
+        if (GetGesture(MLHandTracking.Right, MLHandTracking.HandKeyPose.Fist))
+        {
+            if (!rightClosed)
+            {
+                OnRightClosed();
+            }
+            rightClosed = true;
+        }
+        else
+        {
+            rightClosed = false;
+        }
+    }
+
+    bool GetGesture(MLHandTracking.Hand hand, MLHandTracking.HandKeyPose type)
+    {
+        if (hand != null)
+        {
+            if (hand.KeyPose == type && hand.HandKeyPoseConfidence > confidenceThreshold)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void OnRightClosed()
+    {
+        Debug.Log("Right hand closed.");
+    }
+
+    public void OnLeftClosed()
+    {
+        Debug.Log("Left hand closed.");
+    }
+}
